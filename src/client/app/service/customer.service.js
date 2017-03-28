dcceff325d4805c66d57a26cda0b3c699734b680
NingLuo <luo.n@husky.neu.edu>
2017-03-28 09:53:56 -0400

    modified customer service on client side, removed commented code

--- src/client/app/service/customer.service.js
+++ src/client/app/service/customer.service.js
@@ -7,22 +7,11 @@
     var module = angular.module("service.module");
 
     module.factory("customerService", customerService);
-    // customerService.$inject = ["$http"];
-    //
-    // function customerService ($http) {
-    //     var api = {
-    //         getCustomers: getCustomers
-    //     };
-    //     return api;
-    //
-    //     function getCustomers() {
-    //         return $http.get('customer.json');
-    //     }
-    // }
 
     customerService.$inject = ["$resource"];
+    ///////////////////////////////////////////
+
     function customerService($resource) {
-        // return $resource('customer.json');
         return $resource('/api/customers/:customerId');
     }
 })();
